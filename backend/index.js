a094d5f71add37565bc4ee65367ba2ba2b80d718
princechouhan1710 <princechouhan1710@gmail.com>
2026-01-27 14:31:02 +0530

    fourth commit

---
+++ backend/index.js
@@ -0,0 +1,54 @@
+let express = require("express");
+const errorhandler = require("./middlewares/errorhandler.js");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+
+let app = express();
+
+const ConnectDB = require("./config/db.js")
+require("dotenv").config();
+
+ConnectDB()
+
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+
+
+app.use(cors({
+  origin: ["http://127.0.0.1:5500/","http://localhost:5173","http://localhost:5174","http://localhost:5175","http://localhost:5176"]
+}));
+
+const path = require("path");
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+
+
+let UserRoute = require("./routes/userRoutes.js");
+let movieRoute = require("./routes/movieRoute.js");
+let theatresRoute = require("./routes/theatresRoute.js");
+let showRoute = require("./routes/showRoute.js");
+
+app.use("/api/user/", UserRoute)
+app.use("/api/movie", movieRoute)
+app.use("/api/theatres", theatresRoute)
+app.use("/api/show", showRoute)
+
+//for serving the folder to the port
+app.use(express.static(path.join(__dirname,"../Frontend/dist")))
+
+//for the main file
+app.get("*",(req,res)=>{
+  res.sendFile(path.resolve(__dirname,"../Frontend/dist/index.html"))
+})
+
+
+app.use(errorhandler)
+
+app.listen(4000, (err) => {
+  console.log(err || "Server Run Port 4000")
+})
+
+
